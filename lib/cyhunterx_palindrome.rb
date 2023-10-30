6edd1e315804ec9b845b89f8447a30344c34c5b6
CyHunterX <hunterx.ck@gmail.com>
2023-10-30 13:53:05 -0400

    Initialize repository

---
+++ lib/cyhunterx_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "cyhunterx_palindrome/version"
+
+module CyhunterxPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/cyhunterx_palindrome/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module CyhunterxPalindrome
+  VERSION = "0.1.0"
+end
